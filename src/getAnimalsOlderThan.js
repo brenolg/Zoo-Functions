9104aa8a0fd1919d9f55b2b34e34b6bf5b293dc1
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2022-08-27 21:12:27 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+    const specieAge = data.species.find((specie) => specie.name === animal);
+    return specieAge.residents.every((i) => i.age >= age);
+  
 }
 
 module.exports = getAnimalsOlderThan;
+
+
+
