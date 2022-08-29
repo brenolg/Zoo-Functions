95a57b5dd769d2b917a7234c4e7a438756840a18
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2022-08-29 17:02:21 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const employe = data.employees.find((employe) => employe.firstName === employeeName || employe.lastName === employeeName);
+  if (employeeName === null || employeeName === undefined){
+    return {}
+  }
+  return employe
 }
 
 module.exports = getEmployeeByName;
