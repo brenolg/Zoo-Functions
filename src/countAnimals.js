f1e3416e66b239fd1a82c51346da86743bad10ff
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2022-08-30 13:18:46 -0300

    countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,30 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+    if (animal === undefined) {
+    const animalTotal = {};
+    data.species.forEach((specie) => {
+      animalTotal[specie.name] = specie.residents.length
+    
+    });
+    return animalTotal;
+  }
+
+  if (animal.sex === undefined) {
+    const species = data.species.find((specie) => animal.specie === specie.name);
+    animalNumber = species.residents.length;
+  return animalNumber;
+
+  }
+  const speciesSex = data.species.find((specie) => animal.specie === specie.name).residents;
+  const sexAnimal = speciesSex.filter((sexChosen) => sexChosen.sex === animal.sex);
+  return sexAnimal.length;
+  
+  
 }
 
 module.exports = countAnimals;
+
+
+
