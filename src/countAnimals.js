36ce77fa0165fa8d8132b96165b5b2eddc66a76f
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2022-08-31 15:16:45 -0300

    requisito 8

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -6,16 +6,13 @@ function countAnimals(animal) {
     const animalTotal = {};
     data.species.forEach((specie) => {
       animalTotal[specie.name] = specie.residents.length
-    
     });
     return animalTotal;
   }
-
   if (animal.sex === undefined) {
     const species = data.species.find((specie) => animal.specie === specie.name);
     animalNumber = species.residents.length;
   return animalNumber;
-
   }
   const speciesSex = data.species.find((specie) => animal.specie === specie.name).residents;
   const sexAnimal = speciesSex.filter((sexChosen) => sexChosen.sex === animal.sex);
