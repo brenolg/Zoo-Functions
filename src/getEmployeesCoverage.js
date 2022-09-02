6fe1cad01ea0512068abe9e0e08586170178d458
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2022-09-02 16:49:04 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,60 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+
+const employeeName = (nome) => employees
+  .find((employee) => employee.firstName === nome || employee.lastName === nome);
+
+const employeeId = (id) => employees.find((employee) => employee.id === id);
+
+const validaçãoEmpregado = (empreg) => {
+  const info = (Object.keys(empreg).includes('id')) ? employeeId(empreg.id)
+    : employeeName(empreg.name);
+  return info;
+};
+
+const animaisTrabalhados = (idAnimais) => {
+  const animais = [];
+  const local = [];
+  species.forEach((animal) => {
+    if (idAnimais.includes(animal.id)) {
+      animais.push(animal.name);
+      local.push(animal.location);
+    }
+  });
+  return [animais, local];
+};
+
+const objGenerator = (empregado, animais) => ({ id: empregado.id,
+  fullName: `${empregado.firstName} ${empregado.lastName}`,
+  species: animais[0],
+  locations: animais[1],
+});
+
+const todosFuncionarios = () => {
+  const funcionariosInfo = [];
+  employees.forEach((funcionario) => {
+    const idInfo = validaçãoEmpregado(funcionario);
+    const idAnimais = idInfo.responsibleFor;
+    const animais = animaisTrabalhados(idAnimais);
+    const coberturaEmpregado = objGenerator(idInfo, animais);
+    funcionariosInfo.push(coberturaEmpregado);
+  });
+  return funcionariosInfo;
+};
+
+function getEmployeesCoverage(empregado) {
+  if (empregado !== undefined) {
+    const idInfo = validaçãoEmpregado(empregado);
+    if (idInfo === undefined && empregado !== undefined) {
+      throw new Error('Informações inválidas');
+    }
+    const idAnimais = idInfo.responsibleFor;
+    const animais = animaisTrabalhados(idAnimais);
+    const coberturaEmpregado = objGenerator(idInfo, animais);
+    return coberturaEmpregado;
+  }
+  return todosFuncionarios();
 }
 
 module.exports = getEmployeesCoverage;
