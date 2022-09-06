588c4c0c481b282a5b871f59d5877221bc839ae5
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2022-09-06 15:23:56 -0300

    handlerElephants2

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,28 +1,31 @@
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
-  it('retorna a quantidade de elefantes', () => {
-    const numbersOfElephants = 4;
-    expect(numbersOfElephants).toEqual(4);
+  it('Retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toEqual(4);
   });
-  it('retorna um array com a relação dos nomes de todos os elefantes', () => {
+  it('Retorna um array com a relação dos nomes de todos os elefantes', () => {
     expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
   });
-  it('retorna a media de idade dos elefantes', () => {
+  it('Retorna a media de idade dos elefantes', () => {
     expect(handlerElephants('averageAge')).toBe(10.5);
   });
-  it('retorna a localização dos elefantes dentro do Zoológico', () => {
+  it('Retorna a localização dos elefantes dentro do Zoológico', () => {
     expect(handlerElephants('location')).toEqual('NW');
   });
-  it('retorna a popularidade dos elefantes', () => {
+  it('Retorna a popularidade dos elefantes', () => {
     expect(handlerElephants('popularity')).toEqual(5);
   });
-  it('retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+  it('Retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
     expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
   });
   it('Não passando argumentos a função deve retornar undefined', () => {
-    const actual = handlerElephants();
-    const expected = undefined;
-    expect(actual).toEqual(expected);
+    expect(handlerElephants()).toEqual(undefined);
+  });
+  it('Se o argumento passado for {} deve retornar uma string', () => {
+    expect(handlerElephants({})).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it(('Retorno correto da função handlerElephant pega os parametros corretos'), () => {
+    expect(handlerElephants('id')).toEqual('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5');
   });
 });
